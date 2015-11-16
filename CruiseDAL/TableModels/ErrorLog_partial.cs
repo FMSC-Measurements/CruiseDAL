df34d9f37ff5502efb64eb0d7a0d799ca1761dbf
Ben Campbell <benjaminjcampbell@fs.fed.us>
2015-11-16 14:14:27 -0700

    add RowID prop to ErrorLog

---
+++ CruiseDAL/TableModels/ErrorLog_partial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using FMSC.ORM.Core.EntityAttributes;
+
+namespace CruiseDAL.TableModels
+{
+    public partial class ErrorLogDO
+    {
+        [PrimaryKeyField(FieldName="RowID")]
+        public long? RowID {get; set;}
+    }
+}
