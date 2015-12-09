517f1df5df05f694aba87e282514cfca1b16f49b
Ben Campbell <benjaminjcampbell@fs.fed.us>
2015-12-08 18:07:17 -0700

    check in

--- FMSC.ORM/Core/SQL/SQLSelectBuilder.cs
+++ FMSC.ORM/Core/SQL/SQLSelectBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,13 +9,22 @@ using FMSC.ORM.NetCF;
 
 namespace FMSC.ORM.Core.SQL
 {
+    public delegate IEnumerable SelectExecutor(SQLSelectBuilder builder);
+    public delegate IEnumerable<T> SelectExecutor<out T>(SQLSelectBuilder builder);
+
     public class SQLSelectBuilder
     {
+
+        public SelectExecutor Executor { get; set; }
 
         public SelectSource Source { get; set; }
         public ResultColumnCollection ResultColumns { get; set; }
         public SelectClause Clause { get; set; }
 
+        public IEnumerable Execute()
+        {
+            return Executor(this);
+        }
 
         public string ToSQL()
         {
