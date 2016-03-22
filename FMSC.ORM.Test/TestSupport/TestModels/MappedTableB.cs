2d770148f27ffcf258790015a48743e95851eafc
Ben Campbell <benjaminjcampbell@fs.fed.us>
2016-03-22 15:06:29 -0600

    add types for testing mapping tables

---
+++ FMSC.ORM.Test/TestSupport/TestModels/MappedTableB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMSC.ORM.TestSupport.TestModels
+{
+    public class MappedTableB
+    {
+        public long? ID { get; set; }
+        public string Data { get; set; }
+    }
+}
---
+++ FMSC.ORM.Test/TestSupport/TestModels/MappingAB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMSC.ORM.TestSupport.TestModels
+{
+    public class MappingAB
+    {
+        public long? ID { get; set; }
+        public long? ID_A { get; set; }
+        public long? ID_B { get; set; }
+        public string Data { get; set; }
+    }
+}
