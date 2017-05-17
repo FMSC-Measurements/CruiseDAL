b22c72f6291b4be3dad431fd26a077589dc45470
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-05-17 15:00:56 -0600

    remove dependencies on FMSC.Core and add LinqBridge

---
+++ FMSC.ORM/NetCF/StringBuilderExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace System.Text
+{
+    internal static class StringBuilderExtentions
+    {
+        public static void AppendLine(this StringBuilder @this)
+        {
+            @this.Append("\r\n");
+        }
+
+        public static void AppendLine(this StringBuilder @this, string value)
+        {
+            @this.Append(value + "\r\n");
+        }
+    }
+}
