48cd65ed5335827dc002c0ed8e9bbea63fc01671
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-05-25 15:18:08 -0600

    remove dependency on FMSC.Core

---
+++ CruiseDAL/NetCF/ExtensionAttribute.cs
@@ -0,0 +1,5 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
+    internal sealed partial class ExtensionAttribute : Attribute { }
+}
