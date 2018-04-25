789854d7850745a6ab587d031304cffdd5991b26
Ben Campbell <benjaminjcampbell@fs.fed.us>
2018-04-25 14:29:40 -0400

    increment version id to 2.0.0.3

--- FMSC.ORM/Properties/AssemblyInfo.cs
+++ FMSC.ORM/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.3")]
 
 #if !NetCF
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.3")]
 #endif
