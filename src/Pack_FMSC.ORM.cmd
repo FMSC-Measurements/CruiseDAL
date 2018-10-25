51221c810da625c47eed500a494ea9aa6ce61985
Ben Campbell <benjaminjcampbell@fs.fed.us>
2018-10-25 13:54:05 -0400

    update build configs

---
+++ src/Pack_FMSC.ORM.cmd
@@ -0,0 +1,25 @@
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+
+::Boilderplate 
+::detect if invoked via Window Explorer
+SET interactive=1
+ECHO %CMDCMDLINE% | FIND /I "/c" >NUL 2>&1
+IF %ERRORLEVEL% == 0 SET interactive=0
+
+::name of this script
+SET me=%~n0
+::directory of script
+SET parent=%~dp0
+
+IF NOT DEFINED build_config SET build_config="Release"
+
+IF NOT DEFINED packageOutputDir SET packageOutputDir=%parent%..\PackageOutput
+
+dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%FMSC.ORM\FMSC.ORM.csproj
+dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%FMSC.ORM.Legacy\FMSC.ORM.Legacy.csproj
+
+::if invoked from windows explorer, pause
+IF "%interactive%"=="0" PAUSE
+ENDLOCAL
+EXIT /B 0
