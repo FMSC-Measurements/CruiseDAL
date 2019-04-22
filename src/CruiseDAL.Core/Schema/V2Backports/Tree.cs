4d8c34e746859579b3420912fc90b6d00d13a8e8
Ben Campbell <benjocamps@gmail.com>
2019-04-22 13:55:42 -0400

    fix Tree view

--- src/CruiseDAL.Core/Schema/V2Backports/Tree.cs
+++ src/CruiseDAL.Core/Schema/V2Backports/Tree.cs
@@ -4,74 +4,14 @@
     {
         public const string CREATE_VIEW_TREE =
             "CREATE VIEW Tree AS " +
-            "WITH plotTrees AS ( " +
-            "SELECT " +
-                "t.Tree_CN, " +
-                "t.TreeID AS Tree_GUID, " +
-                "cu.CuttingUnit_CN, " +
-                "st.Stratum_CN, " +
-                "sg.SampleGroup_CN, " +
-                "tdv.TreeDefaultValue_CN,  " +
-                "plt.Plot_Stratum_CN AS Plot_Stratum_CN, " +
-                "t.Species, " +
-                "t.LiveDead, " +
-                "t.TreeNumber, " +
-                "t.CountOrMeasure, " +
-                "tcv.ExpansionFactor, " +
-                "tcv.TreeFactor, " +
-                "tcv.PointFactor, " +
-                "t.XCoordinate, " +
-                "t.YCoordinate, " +
-                "t.ZCoordinate " +
-            "FROM Tree_V3 AS t " +
-            "JOIN SampleGroup_V3 AS sg ON t.StratumCode = sg.StratumCode AND t.SampleGroupCode = sg.SampleGroupCode  " +
-            "JOIN Stratum AS st ON t.StratumCode = st.Code " +
-            "JOIN CuttingUnit AS cu ON t.CuttingUnitCode = cu.Code " +
-            "JOIN Plot_Stratum AS plt USING (StratumCode, PlotNumber) " +
-            "LEFT JOIN TreeCalculatedValues AS tcv USING (Tree_CN) " +
-            "LEFT JOIN TreeDefaultValue AS tdv ON " +
-                        "tdv.Species = t.Species " +
-                        "AND tdv.LiveDead = t.LiveDead " +
-                        "AND tdv.PrimaryProduct = sg.PrimaryProduct " +
-            "), " +
-            "nonPlotTrees AS ( " +
-            "SELECT " +
-                "t.Tree_CN, " +
-                "t.TreeID AS Tree_GUID, " +
-                "cu.CuttingUnit_CN, " +
-                "st.Stratum_CN, " +
-                "sg.SampleGroup_CN, " +
-                "tdv.TreeDefaultValue_CN,  " +
-                "null AS Plot_Stratum_CN, " +
-                "t.Species, " +
-                "t.LiveDead, " +
-                "t.TreeNumber, " +
-                "t.CountOrMeasure, " +
-                "tcv.ExpansionFactor, " +
-                "tcv.TreeFactor, " +
-                "tcv.PointFactor, " +
-                "XCoordinate," +
-                "YCoordinate," +
-                "ZCoordinate " +
-            "FROM Tree_V3 AS t " +
-            "JOIN SampleGroup_V3 AS sg ON t.StratumCode = sg.StratumCode AND t.SampleGroupCode = sg.SampleGroupCode  " +
-            "JOIN Stratum AS st ON t.StratumCode = st.Code " +
-            "JOIN CuttingUnit AS cu ON t.CuttingUnitCode = cu.Code " +
-            "LEFT JOIN TreeCalculatedValues AS tcv USING (Tree_CN) " +
-            "LEFT JOIN TreeDefaultValue AS tdv ON " +
-                        "tdv.Species = t.Species " +
-                        "AND tdv.LiveDead = t.LiveDead " +
-                        "AND tdv.PrimaryProduct = sg.PrimaryProduct " +
-            "WHERE t.PlotNumber IS NULL " +
-            ") " +
 
             "SELECT " +
-                "Tree_CN, " +
-                "Tree_GUID, " +
-                "CuttingUnit_CN, " +
-                "Stratum_CN, " +
-                "SampleGroup_CN, " +
-                "TreeDefaultValue_CN,  " +
+                "t.Tree_CN, " +
+                "t.TreeID AS Tree_GUID, " +
+                "cu.CuttingUnit_CN, " +
+                "st.Stratum_CN, " +
+                "sg.SampleGroup_CN, " +
+                "tdv.TreeDefaultValue_CN,  " +
                 "Plot_Stratum_CN AS Plot_CN, " +
                 "t.Species, " +
                 "t.LiveDead, " +
@@ -85,9 +25,9 @@
                     "AS REAL) AS TreeCount, " + // in v2 TreeCount and kpi had a type of REAL
                 "CAST (ifnull(tl.KPI, 0) AS REAL) AS KPI, " +
                 "(CASE ifnull(tl.STM, 0) WHEN 0 THEN 'N' WHEN 1 THEN 'Y' END) AS STM, " +
-                "ExpansionFactor, " +
-                "TreeFactor, " +
-                "PointFactor, " +
+                "tcv.ExpansionFactor, " +
+                "tcv.TreeFactor, " +
+                "tcv.PointFactor, " +
                 "ifnull(t.XCoordinate, 0.0) AS XCoordinate," +
                 "ifnull(t.YCoordinate, 0.0) AS YCoordinate," +
                 "ifnull(t.ZCoordinate, 0.0) AS ZCoordinate, " +
@@ -96,14 +36,14 @@
             "JOIN SampleGroup_V3 AS sg ON t.StratumCode = sg.StratumCode AND t.SampleGroupCode = sg.SampleGroupCode  " +
             "JOIN Stratum AS st ON t.StratumCode = st.Code " +
             "JOIN CuttingUnit AS cu ON t.CuttingUnitCode = cu.Code " +
-            "JOIN Plot_Stratum AS plt USING (StratumCode, PlotNumber) " +
+            "LEFT JOIN Plot_Stratum AS plt USING (StratumCode, PlotNumber) " +
             "LEFT JOIN TreeCalculatedValues AS tcv USING (Tree_CN) " +
             "LEFT JOIN TreeDefaultValue AS tdv ON " +
                         "tdv.Species = t.Species " +
                         "AND tdv.LiveDead = t.LiveDead " +
                         "AND tdv.PrimaryProduct = sg.PrimaryProduct " +
-            "LEFT JOIN TallyLedger AS tl ON tl.TreeID = t.Tree_GUID " +
-            "LEFT JOIN TreeMeasurment AS tm ON t.Tree_GUID = tm.TreeID " +
+            "LEFT JOIN TallyLedger AS tl ON tl.TreeID = t.TreeID " +
+            "LEFT JOIN TreeMeasurment AS tm ON t.TreeID = tm.TreeID " +
             ";";
 
         public const string CTEATE_TRIGGER_TREE_ONUPDATE =
