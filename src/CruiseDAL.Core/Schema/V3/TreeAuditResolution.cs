18d3226abfddad05b81308e3c7a763ac5c52a88f
Ben Campbell <benjocamps@gmail.com>
2019-05-23 13:14:22 -0400

    add unique constraint to TreeAuditResolution

--- src/CruiseDAL.Core/Schema/V3/TreeAuditResolution.cs
+++ src/CruiseDAL.Core/Schema/V3/TreeAuditResolution.cs
@@ -4,10 +4,14 @@
     {
         public const string CREATE_TABLE_TREEAUDITRESOLUTION =
 @"CREATE TABLE TreeAuditResolution (
+    TreeAuditResolution_CN INTEGER PRIMARY KEY AUTOINCREMENT, 
     TreeID TEXT NOT NULL,
     TreeAuditRuleID TEXT NOT NULL,
     Resolution TEXT,
     Initials TEXT,
+
+    UNIQUE (TreeID, TreeAuditRuleID),
+
     FOREIGN KEY (TreeID) REFERENCES Tree_V3 (TreeID) ON DELETE CASCADE,
     FOREIGN KEY (TreeAuditRuleID) REFERENCES TreeAuditRule (TreeAuditRuleID) ON DELETE CASCADE
 );";
