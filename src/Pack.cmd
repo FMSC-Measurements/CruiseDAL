dc150095d09cae458e43bec34be6064467af6f90
Ben Campbell <benjocamps@gmail.com>
2019-06-07 14:04:22 -0400

    update build instr

--- src/Pack.cmd
+++ src/Pack.cmd
@@ -17,7 +17,11 @@ IF NOT DEFINED build_config SET build_config="Release"
 IF NOT DEFINED packageOutputDir SET packageOutputDir=%parent%..\PackageOutput
 
 dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%FMSC.ORM\FMSC.ORM.csproj
-dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%FMSC.ORM.Legacy\FMSC.ORM.Legacy.csproj
+dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%FMSC.ORM.ModelsGenerator\FMSC.ORM.ModelsGenerator.csproj
+dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%CruiseDAL.Core\CruiseDAL.Core.csproj
+dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%CruiseDAL.V2\CruiseDAL.V2.csproj
+dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%CruiseDAL.V3\CruiseDAL.V3.csproj
+dotnet pack -c %build_config% --include-source -o %packageOutputDir% %parent%CruiseDAL.V3.Models\CruiseDAL.V3.Models.csproj
 
 ::if invoked from windows explorer, pause
 IF "%interactive%"=="0" PAUSE
