c9139a88ae6e0f5036c42afccae8aa2d79f3bfeb
Ben Campbell <benjocamps@gmail.com>
2019-07-02 11:37:23 -0400

    add GetConvertedPath and new Migrate method

--- src/CruiseDAL.V3/Migrator.cs
+++ src/CruiseDAL.V3/Migrator.cs
@@ -8,7 +8,7 @@ namespace CruiseDAL
 {
     public class Migrator
     {
-        public static string MigrateFromV2ToV3(string v2Path, bool overwrite = false)
+        public static string GetConvertedPath(string v2Path)
         {
             // get path, directory and filename of original file
             var fileDirectory = System.IO.Path.GetDirectoryName(v2Path);
@@ -18,17 +18,30 @@ namespace CruiseDAL
             var newFileName = fileName + ".crz3";
             var newFilePath = System.IO.Path.Combine(fileDirectory, newFileName);
 
+            return newFilePath;
+        }
+
+        public static string MigrateFromV2ToV3(string v2Path, bool overwrite = false)
+        {
+            var newFilePath = GetConvertedPath(v2Path);
+            var newFileName = System.IO.Path.GetFileName(newFilePath);
+
             // check temp file doesn't already exist, otherwise throw exception
             var fileAlreadyExists = File.Exists(newFilePath);
             if (fileAlreadyExists && !overwrite) { throw new UpdateException(newFileName + " already exists"); }
 
+            
+
+            return newFilePath;
+        }
+
+        public static void MigrateFromV2ToV3(string v2Path, string newFilePath)
+        {
             using (var v2Cruise = new CruiseDatastore(v2Path, false, null, new Updater_V2()))
             using (var newCruise = new CruiseDatastore_V3(newFilePath, true))
             {
                 MigrateFromV2ToV3(v2Cruise, newCruise);
             }
-
-            return newFilePath;
         }
 
         public static void MigrateFromV2ToV3(CruiseDatastore v2db, CruiseDatastore_V3 v3db)
