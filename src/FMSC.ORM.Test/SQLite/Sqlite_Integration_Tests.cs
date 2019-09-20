3b3695f4128849c0cc895a749b61c4f272c86483
Ben Campbell <benjocamps@gmail.com>
2019-09-20 09:33:53 -0400

    add more Sqlite tests

--- src/FMSC.ORM.Test/SQLite/Sqlite_Integration_Tests.cs
+++ src/FMSC.ORM.Test/SQLite/Sqlite_Integration_Tests.cs
@@ -74,12 +74,52 @@ namespace FMSC.ORM.Test.SQLite
                 var result = command.ExecuteScalar();
 
                 result.Should().BeOfType(typeof(Byte[]));
+                var result_byteA = result as Byte[];
+                result_byteA.Should().BeEquivalentTo(guid.ToByteArray());
 
-                Assert.Equal(guid.ToByteArray(), result);
+                var result_guid = new Guid(result_byteA);
+                result_guid.Should().Be(guid);
+                
             }
         }
 
         [Fact]
+        // expected behavior is that the datetime values is converted to 
+        // a string when used in a parameter. format is in ISO-8601 https://en.wikipedia.org/wiki/ISO_8601
+        public void Echo_value_datetime()
+        {
+            var pName = "@p1";
+            var expectedValue = DateTime.Now;
+
+            using (var connection = DbProvider.CreateConnection())
+            {
+                connection.ConnectionString = "Data Source=:memory:";
+
+                var command = connection.CreateCommand();
+                command.CommandText = $"SELECT {pName};";
+                var parm = command.CreateParameter();
+                parm.ParameterName = pName;
+                parm.Value = expectedValue;
+                command.Parameters.Add(parm);
+
+                connection.Open();
+
+                var result = command.ExecuteScalar();
+                Output.WriteLine(result.ToString());
+
+                result.Should().BeOfType(typeof(string));
+                var resultDT = DateTime.Parse(result as string);
+                expectedValue.Should().Be(resultDT);
+            }
+        }
+
+        [Fact]
+        // expected behavor is that the guid is converted to a byte array
+        // when add to a command as a paramiter. 
+        // when read back as a string it will be a jiberish string.
+        // although, this string will be the same as the string we get when 
+        // we convert the byte array of guid to a string, we can not convert 
+        // the result string to a byte array and then back to a guid. At least I tryed
         public void Echo_value_guid_as_str()
         {
             var pName = "@p1";
@@ -102,11 +142,19 @@ namespace FMSC.ORM.Test.SQLite
                 var result = command.ExecuteScalar();
 
                 result.Should().BeOfType(typeof(string));
-
+                var result_str = result as string;
+                
                 LogEncoding(connection);
 
-                var guid_str = Encoding.UTF8.GetString(guid.ToByteArray());
-                Assert.Equal(guid_str, result);
+                var guid_byteArr = guid.ToByteArray();
+                var guid_str = Encoding.Default.GetString(guid_byteArr);
+                guid_str.Should().Be(result_str);
+
+
+                ////var result_byteArr = result_str.Select(x => (Byte)x).ToArray();
+                //var result_byteArr = Encoding.Default.GetBytes(result_str);
+                //var result_guid = new Guid(result_byteArr);
+                //result_guid.Should().Be(guid);
             }
         }
 
