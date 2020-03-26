c7b1366e9347f0400d9f796364145a53029373a8
Ben Campbell <benjocamps@gmail.com>
2020-03-26 16:21:13 -0400

    Update ValueMapper.cs
    return null if reading invalid value when reading nullable enum

--- src/FMSC.ORM/Core/ValueMapper.cs
+++ src/FMSC.ORM/Core/ValueMapper.cs
@@ -63,6 +63,7 @@ namespace FMSC.ORM.Core
                 }
                 catch
                 {
+                    if (isNullable) { return null; }
                     return Activator.CreateInstance(targetType);
                 }
             }
