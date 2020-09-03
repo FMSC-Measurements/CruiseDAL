888b90506f351bb79c2cc086dcd4d52d03b8d7b1
Ben Campbell <benjocamps@gmail.com>
2020-09-03 17:19:10 -0400

    implementing TreeDefault Redesign WIP

---
+++ src/CruiseDAL.V3/Schema/Cruise/Species.cs
@@ -0,0 +1,27 @@
+﻿namespace CruiseDAL.Schema
+{
+    public partial class DDL
+    {
+        public const string CREATE_TABLE_Species =
+@"CREATE TABLE Species (
+    Species_cn INTEGER PRIMARY KEY AUTOINCREMENT,
+    SpeciesCode TEXT COLLATE NOCASE, 
+    CruiseID TEXT NOT NULL COLLATE NOCASE,
+    ContractSpecies TEXT, 
+    CHECK (length(SpeciesCode) > 0),
+    
+    UNIQUE (SpeciesCode, CruiseID)
+); ";
+    }
+
+    public partial class Migrations
+    {
+        public const string MIGRATE_SPECIESCODE_FROM_TREEDEFAULTVALUE =
+@"INSERT INTO {0}.Species ( 
+    SpeciesCode,
+    CruiseID,
+    ContractSpecies
+) 
+SELECT Species,  '{3}', ContractSpecies FROM {1}.TreeDefaultValue GROUP BY Species;";
+    }
+}
--- src/CruiseDAL.V3/Schema/Cruise/TreeDefaultValue.cs
+++ src/CruiseDAL.V3/Schema/Cruise/TreeDefaultValue.cs
@@ -6,17 +6,18 @@
 @"CREATE TABLE TreeDefaultValue ( 
     TreeDefaultValue_CN INTEGER PRIMARY KEY AUTOINCREMENT, 
     CruiseID TEXT NOT NULL COLLATE NOCASE,
-    PrimaryProduct TEXT NOT NULL COLLATE NOCASE, 
-    SpeciesCode TEXT NOT NULL COLLATE NOCASE, 
-    LiveDead TEXT NOT NULL COLLATE NOCASE, 
-    FIAcode INTEGER DEFAULT 0, 
+    SpeciesCode TEXT COLLATE NOCASE,
+    SampleGroupPattern TEXT COLLATE NOCASE,
+    PrimaryProduct TEXT COLLATE NOCASE,
     CullPrimary REAL DEFAULT 0.0, 
-    HiddenPrimary REAL DEFAULT 0.0, 
+    CullPrimaryDead REAL DEFAULT 0.0,
+    HiddenPrimary REAL DEFAULT 0.0,
+    HiddenPrimaryDead REAL DEFAULT 0.0,
+    TreeGrade TEXT DEFAULT '0' COLLATE NOCASE,
+    TreeGradeDead TEXT DEFAULT '0' COLLATE NOCASE,
     CullSecondary REAL DEFAULT 0.0, 
     HiddenSecondary REAL DEFAULT 0.0,
     Recoverable REAL DEFAULT 0.0, 
-    ContractSpecies TEXT, 
-    TreeGrade TEXT DEFAULT '0' COLLATE NOCASE, 
     MerchHeightLogLength INTEGER DEFAULT 0,
     MerchHeightType TEXT DEFAULT 'F' , 
     FormClass REAL DEFAULT 0.0, 
@@ -28,13 +29,16 @@
     ModifiedBy TEXT, 
     ModifiedDate DateTime, 
     RowVersion INTEGER DEFAULT 0, 
-    UNIQUE (CruiseID, PrimaryProduct, SpeciesCode, LiveDead), 
+    UNIQUE (CruiseID, PrimaryProduct, SpeciesCode), 
 
     CHECK (LiveDead IN ('L', 'D')),
 
     FOREIGN KEY (CruiseID) REFERENCES Cruise (CruiseID) ON DELETE CASCADE,
-    FOREIGN KEY (SpeciesCode) REFERENCES Species (SpeciesCode) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (SpeciesCode, CruiseID) REFERENCES Species (SpeciesCode, CruiseID) ON UPDATE CASCADE ON DELETE CASCADE
 );";
+
+        public const string CREATE_UNIQUE_INDEX_TreeDefaultValue_SpeciesCode_SampleGroupPattern_PrimaryProduct =
+@"CREATE UNIQUE INDEX TreeDefaultValue_SpeciesCode_SampleGroupPattern_PrimaryProduct ON TreeDefaultValue (ifnull(SpeciesCode, ''), ifnull(SampleGroupPattern, ''), ifnull(PrimaryProduct, ''));";
 
         public const string CREATE_INDEX_TreeDefaultValue_SpeciesCode =
             @"CREATE INDEX 'TreeDefaultValue_SpeciesCode' ON 'TreeDefaultValue' ('SpeciesCode');";
@@ -43,88 +47,94 @@
             @"CREATE INDEX 'TreeDefaultValue_CruiseID' ON 'TreeDefaultValue' ('CruiseID');";
 
         public const string CREATE_TRIGGER_TREEDEFAULTVALUE_ONUPDATE =
-            "CREATE TRIGGER TreeDefaultValue_OnUpdate " +
-            "AFTER UPDATE OF " +
-                "PrimaryProduct, " +
-                "SpeciesCode, " +
-                "LiveDead, " +
-                "FIAcode, " +
-                "CullPrimary, " +
-                "HiddenPrimary, " +
-                "CullSecondary, " +
-                "HiddenSecondary, " +
-                "Recoverable, " +
-                "ContractSpecies, " +
-                "TreeGrade, " +
-                "MerchHeightLogLength, " +
-                "MerchHeightType, " +
-                "FormClass, " +
-                "BarkThicknessRatio, " +
-                "AverageZ, " +
-                "ReferenceHeightPercent " +
-            "ON TreeDefaultValue " +
-            "FOR EACH ROW " +
-            "BEGIN " +
-                "UPDATE TreeDefaultValue SET ModifiedDate = datetime( 'now', 'localtime') WHERE TreeDefaultValue_CN = new.TreeDefaultValue_CN; " +
-                "UPDATE TreeDefaultValue SET RowVersion = old.RowVersion + 1 WHERE TreeDefaultValue_CN = old.TreeDefaultValue_CN; " +
-            "END; ";
+@"CREATE TRIGGER TreeDefaultValue_OnUpdate 
+AFTER UPDATE OF 
+    SpeciesCode, 
+    PrimaryProduct, 
+    SampleGroupPattern, 
+    CullPrimary,
+    CullPrimaryDead,
+    HiddenPrimary,
+    HiddenPrimaryDead,
+    TreeGrade,
+    TreeGradeDead,
+    CullSecondary, 
+    HiddenSecondary, 
+    Recoverable, 
+    MerchHeightLogLength, 
+    MerchHeightType, 
+    FormClass, 
+    BarkThicknessRatio, 
+    AverageZ, 
+    ReferenceHeightPercent 
+ON TreeDefaultValue 
+FOR EACH ROW 
+BEGIN 
+    UPDATE TreeDefaultValue SET ModifiedDate = datetime( 'now', 'localtime') WHERE TreeDefaultValue_CN = new.TreeDefaultValue_CN; 
+    UPDATE TreeDefaultValue SET RowVersion = old.RowVersion + 1 WHERE TreeDefaultValue_CN = old.TreeDefaultValue_CN; 
+END; ";
     }
 
     public partial class Migrations
     {
         public const string MIGRATE_TREEDEFAULTVALUE_FORMAT_STR =
-            "INSERT INTO {0}.TreeDefaultValue ( " +
-                    "TreeDefaultValue_CN, " +
-                    "CruiseID," +
-                    "PrimaryProduct, " +
-                    "SpeciesCode, " +
-                    "LiveDead, " +
-                    "FIAcode, " +
-                    "CullPrimary, " +
-                    "HiddenPrimary, " +
-                    "CullSecondary, " +
-                    "HiddenSecondary, " +
-                    "Recoverable, " +
-                    "ContractSpecies, " +
-                    "TreeGrade, " +
-                    "MerchHeightLogLength, " +
-                    "MerchHeightType, " +
-                    "FormClass, " +
-                    "BarkThicknessRatio, " +
-                    "AverageZ, " +
-                    "ReferenceHeightPercent, " +
-                    "CreatedBy, " +
-                    "CreatedDate, " +
-                    "ModifiedBy, " +
-                    "ModifiedDate, " +
-                    "RowVersion " +
-                ") " +
-                "SELECT " +
-                    "TreeDefaultValue_CN, " +
-                    "'{3}', " +
-                    "PrimaryProduct, " +
-                    "Species, " +
-                    "LiveDead, " +
-                    "FIAcode, " +
-                    "CullPrimary, " +
-                    "HiddenPrimary, " +
-                    "CullSecondary, " +
-                    "HiddenSecondary, " +
-                    "Recoverable, " +
-                    "ContractSpecies, " +
-                    "TreeGrade, " +
-                    "MerchHeightLogLength, " +
-                    "MerchHeightType, " +
-                    "FormClass, " +
-                    "BarkThicknessRatio, " +
-                    "AverageZ, " +
-                    "ReferenceHeightPercent, " +
-                    "CreatedBy, " +
-                    "CreatedDate, " +
-                    "ModifiedBy, " +
-                    "ModifiedDate, " +
-                    "RowVersion " +
-                "FROM {1}.TreeDefaultValue;";
+@"INSERT INTO {0}.TreeDefaultValue (
+        TreeDefaultValue_CN,
+        CruiseID,
+        PrimaryProduct,
+        SpeciesCode,
+        CullPrimary,
+        CullPrimaryDead,
+        HiddenPrimary,
+        HiddenPrimaryDead,
+        TreeGrade,
+        TreeGradeDead,
+        CullSecondary,
+        HiddenSecondary,
+        Recoverable,
+        MerchHeightLogLength,
+        MerchHeightType,
+        FormClass,
+        BarkThicknessRatio,        
+        AverageZ, 
+        ReferenceHeightPercent,
+        CreatedBy,
+        CreatedDate,
+        ModifiedBy,
+        ModifiedDate,
+        RowVersion
+    )
+    SELECT
+        tdvl.TreeDefaultValue_CN,
+        '{3}',
+        tdvl.PrimaryProduct,
+        tdvl.Species,
+        tdvl.CullPrimary,
+        tdvd.CullPrimaryDead,
+        tdvl.HiddenPrimary,
+        tdvd.HiddenPrimaryDead,
+        tdvl.TreeGrade,
+        tdvd.TreeGradeDead,
+        tdvl.CullSecondary,
+        tdvl.HiddenSecondary,
+        tdvl.Recoverable,
+        tdvl.MerchHeightLogLength,
+        tdvl.MerchHeightType,
+        tdvl.FormClass,
+        tdvl.BarkThicknessRatio,
+        tdvl.AverageZ,
+        tdvl.ReferenceHeightPercent,
+        tdvl.CreatedBy,
+        tdvl.CreatedDate,
+        tdvl.ModifiedBy,
+        tdvl.ModifiedDate,
+        tdvl.RowVersion
+    FROM (SELECT * FROM {1}.TreeDefaultValue WHERE LiveDead = 'L') AS tdvl 
+    LEFT JOIN (SELECT * FROM {1}.TreeDefaultValue WHERE LiveDead = 'D') AS tdvd USING (Species, PrimaryProduct);
+
+    UPDATE TreeDefaultValue SET 
+    CullPrimaryDead = 
+";
     }
 
     //public partial class Updater
