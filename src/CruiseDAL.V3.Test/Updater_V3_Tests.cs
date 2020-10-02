be433a2897b7b624c6953cb9b0204f92c204d9a7
Ben Campbell <benjocamps@gmail.com>
2020-10-02 17:40:38 -0400

    update v3 updater

--- src/CruiseDAL.V3.Test/Updater_V3_Tests.cs
+++ src/CruiseDAL.V3.Test/Updater_V3_Tests.cs
@@ -1,7 +1,8 @@
-﻿using CruiseDAL.Schema;
-using CruiseDAL.V3.Tests;
-using FluentAssertions;
+﻿using FluentAssertions;
+using FMSC.ORM.Core;
+using FMSC.ORM.SQLite;
 using System;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -37,8 +38,48 @@ namespace CruiseDAL.V3.Test
                 var treeCountAfter = ds.ExecuteScalar<int>("SELECT count(*) FROM Tree_V3;");
                 treeCountAfter.Should().Be(treeCount);
 
-                ds.DatabaseVersion.Should().Be(DDL.DATABASE_VERSION.ToString());
+                ds.DatabaseVersion.Should().Be(CruiseDatastoreBuilder_V3.DATABASE_VERSION.ToString());
                 ds.CheckTableExists("TallyLedger_Tree_Totals");
+            }
+        }
+
+        [Fact]
+        public void ListTablesIntersect()
+        {
+            using (var srcDB = new SQLiteDatastore())
+            using (var destDB = new SQLiteDatastore())
+            {
+                using (var srcConn = srcDB.OpenConnection())
+                using (var destCon = destDB.OpenConnection())
+                {
+                    srcConn.ExecuteNonQuery("CREATE TABLE A ( f1 TEXT );");
+                    srcConn.ExecuteNonQuery("CREATE TABLE B ( f1 TEXT );");
+
+                    destCon.ExecuteNonQuery("CREATE TABLE B ( f1 TEXT );");
+                    destCon.ExecuteNonQuery("CREATE TABLE C ( f1 TEXT );");
+
+                    var tables = Updater_V3.ListTablesIntersect(destCon, srcConn);
+                    tables.Single().Should().Be("B");
+                }
+            }
+        }
+
+        [Fact]
+        public void ListFieldsIntersect()
+        {
+            using (var srcDB = new SQLiteDatastore())
+            using (var destDB = new SQLiteDatastore())
+            {
+                using (var srcConn = srcDB.OpenConnection())
+                using (var destCon = destDB.OpenConnection())
+                {
+                    srcConn.ExecuteNonQuery("CREATE TABLE A ( f1 TEXT, f2 TEXT );");
+
+                    destCon.ExecuteNonQuery("CREATE TABLE A ( f2 TEXT, f3 TEXT );");
+
+                    var tables = Updater_V3.ListFieldsIntersect(destCon, srcConn, "A");
+                    tables.Single().Should().Be("\"f2\"");
+                }
             }
         }
     }
--- src/CruiseDAL.V3/Updater_V3.cs
+++ src/CruiseDAL.V3/Updater_V3.cs
@@ -1,7 +1,16 @@
-﻿namespace CruiseDAL
+﻿using FMSC.ORM.Core;
+using FMSC.ORM.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+
+namespace CruiseDAL
 {
     public class Updater_V3 : IUpdater
     {
+        public static ILogger Logger { get; set; } = LoggerProvider.Get();
+
         public void Update(CruiseDatastore datastore)
         {
             var version = datastore.DatabaseVersion;
@@ -19,12 +28,122 @@
             using (var newDatastore = new CruiseDatastore_V3())
             {
                 // migrate contents of old db into new in-memory database
-                Migrator.Migrate(ds, newDatastore, new[] { "SamplerState", });
+                Migrate(ds, newDatastore, new[] { "SamplerState", });
 
                 // use back up rutine to replace old database with 
                 // migrated contents
                 newDatastore.BackupDatabase(ds);
             }
+        }
+
+        public static void Migrate(CruiseDatastore sourceDS, CruiseDatastore destinationDS, IEnumerable<string> excluding = null)
+        {
+            var destConn = destinationDS.OpenConnection();
+            var sourceConn = sourceDS.OpenConnection();
+
+
+            try
+            {
+                Migrate(sourceConn, destConn, excluding);
+            }
+            finally
+            {
+                destinationDS.ReleaseConnection();
+                sourceDS.ReleaseConnection();
+            }
+        }
+
+        public static void Migrate(DbConnection sourceConn, DbConnection destConn, IEnumerable<string> excluding = null)
+        {
+            var to = "main"; // alias used by the source database
+            var from = "fromdb";
+
+            // get the initial state of foreign keys, used to restore foreign key setting at end of merge process
+            var foreignKeys = destConn.ExecuteScalar<string>("PRAGMA foreign_keys;", null, null);
+            destConn.ExecuteNonQuery("PRAGMA foreign_keys = off;");
+
+#if SYSTEM_DATA_SQLITE
+            var srcDataSource = ((System.Data.SQLite.SQLiteConnection)sourceConn).FileName;
+#else
+            var srcDataSource = sourceConn.DataSource;
+#endif
+            destConn.ExecuteNonQuery($"ATTACH DATABASE \"{srcDataSource}\" AS {from};");
+            try
+            {
+                using (var transaction = destConn.BeginTransaction())
+                {
+                    // get list of all tables that are in both databases
+                    IEnumerable<string> tables = ListTablesIntersect(destConn, sourceConn);
+                    try
+                    {
+                        foreach (var table in tables)
+                        {
+                            if (excluding?.Contains(table) ?? false)
+                            { continue; }
+
+                            if (table == "Globals")
+                            {
+                                destConn.ExecuteNonQuery(
+    $@"INSERT OR IGNORE INTO {to}.{table} (""Block"", ""Key"", ""Value"")
+SELECT ""Block"", ""Key"", ""Value""
+FROM {from}.{table} 
+WHERE ""Block"" != 'Database' AND ""Key"" != 'Version';", null, transaction);
+                                continue;
+                            }
+                            else
+                            {
+                                // get the interscetion of fields in table from both databases 
+                                // encased in double quotes just incase any field names are sql keywords
+                                string[] both = ListFieldsIntersect(sourceConn, destConn, table);
+                                var fields = string.Join(",", both);
+
+                                destConn.ExecuteNonQuery(
+    $@"INSERT OR IGNORE INTO {to}.{table} ({fields})
+SELECT {fields} FROM {from}.{table};"
+                                    , null, transaction);
+
+                            }
+                        }
+
+                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        transaction.Rollback();
+                        Logger.LogException(e);
+                        throw;
+                    }
+                    finally
+                    {
+                        sourceConn.ExecuteNonQuery($"PRAGMA foreign_keys = {foreignKeys};");
+                    }
+                }
+            }
+            finally
+            {
+                destConn.ExecuteNonQuery($"DETACH DATABASE {from};");
+            }
+        }
+
+        public static string[] ListFieldsIntersect(DbConnection sourceConn, DbConnection destConn, string table)
+        {
+            var sourceFields = sourceConn.QueryScalar2<string>($@"SELECT '""' || Name || '""' FROM pragma_table_info('{table}');");
+
+            var destFields = destConn.QueryScalar2<string>($@"SELECT '""' || Name || '""' FROM pragma_table_info('{table}');");
+
+            var both = sourceFields.Intersect(destFields).ToArray();
+            return both;
+        }
+
+        public static string[] ListTablesIntersect(DbConnection conn1, DbConnection conn2)
+        {
+            var query = "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite^_%' ESCAPE '^';";
+
+            var tables1 = conn1.QueryScalar2<string>(query);
+
+            var tables2 = conn2.QueryScalar2<string>(query);
+
+            return tables1.Intersect(tables2).ToArray();
         }
     }
 }
