374d8daba288374516ae472f0955e7c16b583d38
Ben Campbell <benjocamps@gmail.com>
2021-01-21 15:59:11 -0500

    remove backport views

--- src/CruiseDAL.V3/Schema/Cruise/TreeFieldHeading.cs
+++ src/CruiseDAL.V3/Schema/Cruise/TreeFieldHeading.cs
@@ -13,6 +13,10 @@ namespace CruiseDAL.Schema
     CruiseID TEXT NOT NULL COLLATE NOCASE,
     Field TEXT NOT NULL COLLATE NOCASE,
     Heading TEXT NOT NULL, 
+    CreatedBy TEXT DEFAULT 'none',
+    Created_TS DATETIME DEFAULT (CURRENT_TIMESTAMP),
+    ModifiedBy TEXT,
+    Modified_TS DATETIME,
 
     UNIQUE (CruiseID, Field),
 
@@ -26,6 +30,16 @@ namespace CruiseDAL.Schema
 
         public string CreateIndexes => null;
 
-        public IEnumerable<string> CreateTriggers => null;
+        public IEnumerable<string> CreateTriggers => new[] { CREATE_TRIGGER_TreeFieldHeading_OnUpdate };
+
+        public string CREATE_TRIGGER_TreeFieldHeading_OnUpdate =
+@"CREATE TRIGGER TreeFieldHeading_OnUpdate 
+AFTER UPDATE OF
+    Heading
+ON TreeFieldHeading 
+FOR EACH ROW
+BEGIN
+    UPDATE TreeFieldHeading SET Modified_TS = CURRENT_TIMESTAMP WHERE TreeFieldHeading_CN = new.TreeFieldHeading_CN;
+END; ";
     }
 }
