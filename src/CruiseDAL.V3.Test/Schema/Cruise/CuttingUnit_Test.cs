98c813618cff1d841189aa0560927c22a0995bb2
Ben Campbell <benjocamps@gmail.com>
2022-02-11 11:37:23 -0500

    add tests to test cascading unit and stratum code changes

---
+++ src/CruiseDAL.V3.Test/Schema/Cruise/CuttingUnit_Test.cs
@@ -0,0 +1,64 @@
+﻿using CruiseDAL.TestCommon;
+using CruiseDAL.V3.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CruiseDAL.V3.Test.Schema.Cruise
+{
+    public class CuttingUnit_Test : TestBase
+    {
+        public CuttingUnit_Test(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void UpdateUnitCodeCascade()
+        {
+            var init = new DatabaseInitializer();
+            using var db = init.CreateDatabase();
+
+            var tree = new Tree
+            {
+                CruiseID = init.CruiseID,
+                TreeID = Guid.NewGuid().ToString(),
+                TreeNumber = 1,
+                CuttingUnitCode = "u1",
+                StratumCode = "st1",
+                SampleGroupCode = "sg1",
+            };
+            db.Insert(tree);
+
+            var tl = new TallyLedger
+            {
+                CruiseID = init.CruiseID,
+                TallyLedgerID = Guid.NewGuid().ToString(),
+                CuttingUnitCode = "u1",
+                StratumCode = "st1",
+                SampleGroupCode = "sg1",
+                TreeID = tree.TreeID,
+            };
+            db.Insert(tl);
+
+            var unitStratum = db.From<CuttingUnit_Stratum>().Where("CuttingUnitCode = 'u1'").Query().First();
+
+            var unit = db.From<CuttingUnit>().Where("CuttingUnitCode = 'u1'").Query().Single();
+            unit.CuttingUnitCode = "u101";
+            db.Update(unit);
+
+            var unitStratumAgain = db.From<CuttingUnit_Stratum>().Where("CuttingUnit_Stratum_CN = @p1").Query(unitStratum.CuttingUnit_Stratum_CN).Single();
+            unitStratumAgain.CuttingUnitCode.Should().Be(unit.CuttingUnitCode);
+
+            var tlAgain = db.From<TallyLedger>().Query().First();
+            tlAgain.CuttingUnitCode.Should().Be(unit.CuttingUnitCode);
+
+            var treeAgain = db.From<Tree>().Query().First();
+            treeAgain.CuttingUnitCode.Should().Be(unit.CuttingUnitCode);
+        }
+    }
+}
---
+++ src/CruiseDAL.V3.Test/Schema/Cruise/Stratum_Test.cs
@@ -0,0 +1,65 @@
+﻿using CruiseDAL.TestCommon;
+using CruiseDAL.V3.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CruiseDAL.V3.Test.Schema.Cruise
+{
+    public class Stratum_Test : TestBase
+    {
+        public Stratum_Test(ITestOutputHelper output) : base(output)
+        {
+        }
+
+
+        [Fact]
+        public void UpdateStratumCodeCascade()
+        {
+            var init = new DatabaseInitializer();
+            using var db = init.CreateDatabase();
+
+            var tree = new Tree
+            {
+                CruiseID = init.CruiseID,
+                TreeID = Guid.NewGuid().ToString(),
+                TreeNumber = 1,
+                CuttingUnitCode = "u1",
+                StratumCode = "st1",
+                SampleGroupCode = "sg1",
+            };
+            db.Insert(tree);
+
+            var tl = new TallyLedger
+            {
+                CruiseID = init.CruiseID,
+                TallyLedgerID = Guid.NewGuid().ToString(),
+                CuttingUnitCode = "u1",
+                StratumCode = "st1",
+                SampleGroupCode = "sg1",
+                TreeID = tree.TreeID,
+            };
+            db.Insert(tl);
+
+            var unitStratum = db.From<CuttingUnit_Stratum>().Where("StratumCode = 'st1'").Query().First();
+
+            var stratum = db.From<Stratum>().Where("StratumCode = 'st1'").Query().Single();
+            stratum.StratumCode = "st101";
+            db.Update(stratum);
+
+            var unitStratumAgain = db.From<CuttingUnit_Stratum>().Where("CuttingUnit_Stratum_CN = @p1").Query(unitStratum.CuttingUnit_Stratum_CN).Single();
+            unitStratumAgain.StratumCode.Should().Be(stratum.StratumCode);
+
+            var tlAgain = db.From<TallyLedger>().Query().First();
+            tlAgain.StratumCode.Should().Be(stratum.StratumCode);
+
+            var treeAgain = db.From<Tree>().Query().First();
+            treeAgain.StratumCode.Should().Be(stratum.StratumCode);
+        }
+    }
+}
