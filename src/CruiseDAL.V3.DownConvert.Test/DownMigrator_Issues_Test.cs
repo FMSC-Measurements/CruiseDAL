5f1fdf600c15aa6f6a489992291329e6c8dae7dc
Ben Campbell <benjocamps@gmail.com>
2022-10-24 14:18:33 -0500

    [DownMigrator] Fix SaleDownMigrate use SaleNumber instead of SaleID

---
+++ src/CruiseDAL.V3.DownConvert.Test/DownMigrator_Issues_Test.cs
@@ -0,0 +1,37 @@
+﻿using CruiseDAL.TestCommon;
+using CruiseDAL.V3.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CruiseDAL.V3.DownConvert.Test
+{
+    public class DownMigrator_Issues_Test : TestBase
+    {
+        public DownMigrator_Issues_Test(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Goshen_Blueberry_TS()
+        {
+            var fromPath = GetTestFile("52203_Goshen Blueberry_TS_202210201203_GalaxyTabActivePro-8PC5.crz3");
+            var fromDb = new CruiseDatastore_V3(fromPath);
+            var cruiseID = fromDb.From<Cruise>().Query().Single().CruiseID;
+
+            var toPath = GetTempFilePathWithExt(".cruise");
+            var toDb = new DAL(toPath, true);
+
+            var downMigrator = new DownMigrator();
+            downMigrator.MigrateFromV3ToV2(cruiseID, fromDb, toDb);
+
+            var toSale = toDb.From<V2.Models.Sale>().Query().SingleOrDefault();
+            toSale.Should().NotBeNull();
+        }
+    }
+}
