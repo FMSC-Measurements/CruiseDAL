646eb46510f47f7d0a8b2728e97844a109336ba9
Ben Campbell <benjocamps@gmail.com>
2023-01-19 15:13:35 -0600

    FIX [#72] FIA codes not being up converted from V2 templates

--- src/CruiseDAL.V3.UpConvert/Migrators/SpeciesMigrator.cs
+++ src/CruiseDAL.V3.UpConvert/Migrators/SpeciesMigrator.cs
@@ -9,13 +9,32 @@ namespace CruiseDAL.Migrators
     {
         public string MigrateToV3(string toDbName, string fromDbName, string cruiseID, string saleID, string deviceID)
         {
+
+
             return
 $@"INSERT INTO {toDbName}.Species ( 
     SpeciesCode,
     CruiseID,
     ContractSpecies
 ) 
-SELECT Species,  '{cruiseID}', ContractSpecies FROM {fromDbName}.TreeDefaultValue GROUP BY Species;";
+SELECT Species,  '{cruiseID}', ContractSpecies FROM {fromDbName}.TreeDefaultValue GROUP BY Species;
+
+
+WITH sp_fia AS (
+    SELECT sp.SpeciesCode, tdv.FIACode
+    FROM {toDbName}.Species AS sp
+    JOIN ( SELECT Species, FIACode 
+                FROM (SELECT DISTINCT Species, FIACode FROM {fromDbName}.TreeDefaultValue)
+                GROUP BY Species
+                HAVING count(*) = 1 ) AS tdv
+    ON sp.SpeciesCode = tdv.Species
+)
+                
+UPDATE {toDbName}.Species SET 
+    FIACode = (SELECT FIACode FROM sp_fia  WHERE sp_fia.SpeciesCode = SpeciesCode)
+WHERE 
+    SpeciesCode IN (SELECT SpeciesCode FROM sp_fia);
+";
         }
     }
 }
