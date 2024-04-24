3cc2e359f8ec7be7e6c3b5e7610a7abc0cab2b39
Benjamin Campbell <benjamin.campbell@usda.gov>
2024-04-24 12:05:16 -0500

    add test for CommitTransaction and RollbackTransaction

--- src/FMSC.ORM.Test/Core/DatastoreTest.cs
+++ src/FMSC.ORM.Test/Core/DatastoreTest.cs
@@ -1,4 +1,8 @@
-﻿using FMSC.ORM.Test;
+﻿using FluentAssertions;
+using FMSC.ORM.SQLite;
+using FMSC.ORM.Test;
+using FMSC.ORM.TestSupport;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace FMSC.ORM.Core
@@ -7,5 +11,46 @@ namespace FMSC.ORM.Core
     {
         public DatastoreTest(ITestOutputHelper output) : base(output)
         { }
+
+        [Fact]
+        public void CommitTransaction()
+        {
+            using var db = new SQLiteDatastore();
+
+            db.BeginTransaction();
+            db.TransactionDepth.Should().Be(1);
+            db.CurrentTransaction.Should().NotBeNull();
+
+            db.Execute(TestDBBuilder.CREATE_TABLEONE);
+            db.CheckTableExists("TableOne").Should().BeTrue();
+
+
+            db.CommitTransaction();
+            db.TransactionDepth.Should().Be(0);
+            db.CurrentTransaction.Should().BeNull();
+
+            db.CheckTableExists("TableOne").Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void RollbackTransaction()
+        {
+            using var db = new SQLiteDatastore();
+
+            db.BeginTransaction();
+            db.TransactionDepth.Should().Be(1);
+            db.CurrentTransaction.Should().NotBeNull();
+
+            db.Execute(TestDBBuilder.CREATE_TABLEONE);
+            db.CheckTableExists("TableOne").Should().BeTrue();
+
+
+            db.RollbackTransaction();
+            db.TransactionDepth.Should().Be(0);
+            db.CurrentTransaction.Should().BeNull();
+
+            db.CheckTableExists("TableOne").Should().BeFalse();
+        }
     }
 }
